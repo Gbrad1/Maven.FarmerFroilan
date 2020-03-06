aff39f3159156d56dce6140324d5a8d15f8b924c
Giles Bradford <gbradford@Gbrads-Mac.local>
2020-03-06 15:24:14 -0500

    fixed an unknown issue

--- src/main/java/com/zipcodewilmington/froilansfarm/Simulation/Events/WednesdayEvents.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Simulation/Events/WednesdayEvents.java
@@ -1,16 +1,9 @@
 package com.zipcodewilmington.froilansfarm.Simulation.Events;
-import com.zipcodewilmington.froilansfarm.Animal.Horse;
 import com.zipcodewilmington.froilansfarm.Person.Botanist;
-import com.zipcodewilmington.froilansfarm.Person.Farmer;
-import com.zipcodewilmington.froilansfarm.Person.Person;
-import com.zipcodewilmington.froilansfarm.Produce.StrangeFruit;
-import com.zipcodewilmington.froilansfarm.Produce.StrangePlant;
-import com.zipcodewilmington.froilansfarm.Produce.Tomato;
 import com.zipcodewilmington.froilansfarm.Produce.TomatoPlant;
 import com.zipcodewilmington.froilansfarm.Storage.CropRow;
 import com.zipcodewilmington.froilansfarm.Storage.Farm;
 import com.zipcodewilmington.froilansfarm.Storage.Field;
-import com.zipcodewilmington.froilansfarm.Storage.Stable;
 
 public class WednesdayEvents implements Events{
     public void run(Farm farm){
